016fdf4c8407176ca95b109b68e656d09a5834b2
Rishita <125037556+rishitaa04@users.noreply.github.com>
2023-03-19 16:51:32 +0530

    Add files via upload

---
+++ functions.c/properties.c
@@ -0,0 +1,18 @@
+// // Execution always starts from main i.e. compiler execution k liye sbse pelle main function
+// // me jaega
+// // also isme either main function khud call krega doosre function ko ya fir theesra function
+// // call krega doosre function ko
+
+// // main function jisko call krega pelle vhi print hoga pelle
+// // also, there will be multiple functions in a program
+
+// // there are two types of functions i.e. inbuilt (library function ) and user defined (deckared by coder)
+
+// passing arguments i.e. function jo value leta hai usko kehte hn parameter and jo value return krta hai usko
+// return value kehte hn
+
+// Passing arguments :
+
+//         1. void functionname();
+//         2. void printtable(int n);
+//         3. int sum(int a, int b);
