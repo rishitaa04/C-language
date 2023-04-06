c85150da899e5cdf74b3471b74b1341e717743a4
Rishita <125037556+rishitaa04@users.noreply.github.com>
2023-04-06 06:35:16 +0530

    Add files via upload

---
+++ Patterns.c/1.c
@@ -0,0 +1,20 @@
+// *
+// *
+// *
+// *
+
+#include <stdio.h>
+int main()
+{
+    // printf("*****\n");
+    // printf("*****\n");
+    // printf("*****\n");
+    int n;
+    printf("Enter number : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+    return 0;
+}
